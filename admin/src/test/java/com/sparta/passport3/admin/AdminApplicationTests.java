eab31891e1ce3ccd466f39b85031cff9b1e18c68
heejin1023 <heejin1023dev@gmail.com>
2024-10-06 12:31:33 +0900

    .git 제거

---
+++ admin/src/test/java/com/sparta/passport3/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.passport3.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ server/src/test/java/com/sparta/passport3/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.passport3.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
